1f2cc5c90faff31a6fd01683c614cf89ee96370f
Brad Chao <brad@brad.tw>
2018-03-28 10:37:14 +0800

    v2

--- app/src/main/java/tw/brad/boundservicetest/MainActivity.java
+++ app/src/main/java/tw/brad/boundservicetest/MainActivity.java
@@ -1,19 +1,83 @@
 package tw.brad.boundservicetest;
 
+import android.content.ComponentName;
+import android.content.Intent;
+import android.content.ServiceConnection;
+import android.os.Handler;
+import android.os.IBinder;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private boolean isBound = false;    // 預設尚未連接
+    private MyService myService;        // 將會在連接之後取得
+
+    // 開發一個類別實作 ServiceConnection
+    private class MyServiceConnection implements ServiceConnection {
+        // 完成連接
+        @Override
+        public void onServiceConnected(ComponentName componentName, IBinder iBinder) {
+            MyService.LocalBinder binder = (MyService.LocalBinder)iBinder;
+            myService = binder.getMyService();
+            Log.v("brad", "onServiceConnected");
+            isBound = true;
+            myService.setHandler(handler);
+        }
+        // 結束連接
+        @Override
+        public void onServiceDisconnected(ComponentName componentName) {
+            Log.v("brad", "onServiceDisconnected");
+            isBound = false;
+        }
+    }
+    private MyServiceConnection connection = new MyServiceConnection();
+    private MyHandler handler;
+    private TextView mesg;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        handler = new MyHandler();
+        mesg = findViewById(R.id.mesg);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Intent intent = new Intent(this, MyService.class);
+        bindService(intent, connection, BIND_AUTO_CREATE);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (isBound){
+            unbindService(connection);
+        }
     }
 
     public void test1(View view) {
+        myService.doSomething("Brad");
     }
+
     public void test2(View view) {
+        myService.showTextView();
+    }
+
+    class MyHandler extends Handler {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            Log.v("brad", "MainActivity:MyHandler:handlerMessage()");
+            mesg.setText("OK:" + msg.arg1);
+        }
     }
 }
--- app/src/main/java/tw/brad/boundservicetest/MyService.java
+++ app/src/main/java/tw/brad/boundservicetest/MyService.java
@@ -2,18 +2,34 @@ package tw.brad.boundservicetest;
 
 import android.app.Service;
 import android.content.Intent;
+import android.os.Binder;
 import android.os.IBinder;
+import android.os.Message;
 import android.util.Log;
 
 public class MyService extends Service {
+    private final LocalBinder binder = new LocalBinder();
+    private MainActivity.MyHandler handler;
+
+    // Step 2: 一個繼承 Binder 的 public 類別
+    public class LocalBinder extends Binder {
+        MyService getMyService(){
+            Log.v("brad", "getMyService()");
+            return MyService.this;
+        }
+    }
+
     public MyService() {
+        Log.v("brad", "MyService");
     }
 
     // 開始 Bound 綁定服務
     @Override
     public IBinder onBind(Intent intent) {
         Log.v("brad", "onBind");
-        return null;
+
+        //return null; => Step 1
+        return binder;
     }
 
     @Override
@@ -33,4 +49,19 @@ public class MyService extends Service {
         Log.v("brad", "onDestroy");
         super.onDestroy();
     }
+
+    void doSomething(String name){
+        Log.v("brad", "doSomething: " + name);
+    }
+
+    void showTextView(){
+        Message mesg = new Message();
+        mesg.arg1 = (int)(Math.random()*49+1);
+        handler.sendMessage(mesg);
+    }
+
+    void setHandler(MainActivity.MyHandler handler){
+        this.handler = handler;
+    }
+
 }
